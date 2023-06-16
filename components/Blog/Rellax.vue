0df66fea92a29af36f6b946cdc8fa7165225c3a5
saschakrischock <s.krischock@gmail.com>
2023-06-16 15:21:38 +0200

    changes

--- components/Blog/Rellax.vue
+++ components/Blog/Rellax.vue
@@ -8,7 +8,57 @@ export default {
       rellax: null
     };
   },
+
+    
+
   mounted() {
+
+    let timeoutId;
+
+    const startTimer = () => {
+      timeoutId = window.setTimeout(() => {
+        // Slowly scroll down the page
+        scrollDown();
+      }, 5000);
+    };
+
+    const resetTimer = () => {
+      window.clearTimeout(timeoutId);
+      startTimer();
+    };
+
+    const scrollDown = () => {
+      const currentPosition = window.pageYOffset;
+      const targetPosition = document.body.offsetHeight;
+      const distance = targetPosition - currentPosition;
+      const step = Math.ceil(distance / 100); // Adjust scroll speed here
+
+      const scrollAnimation = () => {
+        if (window.pageYOffset < targetPosition) {
+          window.scrollBy(0, step);
+
+          // Request next animation frame
+          window.requestAnimationFrame(scrollAnimation);
+        }
+      };
+
+      // Start the scroll animation
+      window.requestAnimationFrame(scrollAnimation);
+    };
+
+    // Listen for various events to reset the timer and stop scrolling
+    window.addEventListener('scroll', resetTimer);
+    window.addEventListener('mousemove', stopScrolling);
+    window.addEventListener('keypress', stopScrolling);
+
+    const stopScrolling = () => {
+      window.removeEventListener('mousemove', stopScrolling);
+      window.removeEventListener('keypress', stopScrolling);
+    };
+
+    // Start the timer initially
+    startTimer();
+
     this.createRellax();
   },
   beforeUnmount() {

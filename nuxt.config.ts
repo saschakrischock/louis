7ca1d8c3d4965d0be9eed65fe000ec7b1780b8bc
saschakrischock <s.krischock@gmail.com>
2023-12-15 23:15:55 +0100

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,8 +9,12 @@ export default defineNuxtConfig({
       charset: "utf-16",
       viewport: 'width=device-width, initial-scale=1',
       title: "Louis",
-      titleTemplate: "%s - Louis",
+      titleTemplate: "%s",
       meta: [{ name: "description", content: "Louis" }],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        // Replace 'favicon.ico' with the actual filename of your favicon if it's different
+      ]
     },
     
   },

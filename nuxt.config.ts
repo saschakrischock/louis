e47afb48fb4671a98549b0353c560ede801962af
saschakrischock <s.krischock@gmail.com>
2023-12-16 14:17:15 +0100

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
 
     pageTransition: { name: 'page', mode: 'out-in' },
     head: {
-      charset: "utf-16",
+      charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       title: "Louis Gibson",
       titleTemplate: "%s",
